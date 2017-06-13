88ba57ec4c10f0e2375b597741fa8338ccbd7744
D0240295 <fashion0819@yahoo.com.tw>
2017-06-13 19:39:16 +0800

    Good

---
+++ Android_project/app/src/main/java/com/example/norman/android_project/Data.java
@@ -0,0 +1,57 @@
+package com.example.norman.android_project;
+
+
+/**
+ * Created by User on 2017/6/10.
+ */
+
+public class Data {
+    private String classification;
+    private String title;
+    private String content;
+    private String link;
+    private String date;
+
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+}
